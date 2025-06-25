e35ff140655fb58c494c4894dbe07d930670ec10
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-25 18:08:05 +0600

    added react query devtools

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -13,6 +14,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   </React.StrictMode>
 )
