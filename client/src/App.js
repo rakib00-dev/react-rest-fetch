35c9fdc4e3d96cc6ada0a96cad753842c8c6f7b0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-25 18:15:48 +0600

    added the status and isfetching

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query'
 import Form from './components/Form'
 
 function App() {
-  const { data } = useQuery({
+  const { data, isFetching, status } = useQuery({
     queryKey: ['todo'],
     queryFn: async () =>
       await (await fetch('http://localhost:8000/todo')).json(),
@@ -10,9 +10,14 @@ function App() {
 
   console.log(data)
 
+  if (isFetching) {
+    return <h1>loading...</h1>
+  }
+
   return (
     <div className="App">
       <Form />
+      <p>Status: {status}</p>
       {data && data.data && data.data.map((t, i) => <li key={i}>{t.title}</li>)}
     </div>
   )
