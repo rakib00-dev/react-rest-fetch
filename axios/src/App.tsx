294df96c9d830640685ed9c5ad5641c20a92f089
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-26 16:32:22 +0600

    learned the basics of axios

--- axios/src/App.tsx
+++ axios/src/App.tsx
@@ -1,5 +1,32 @@
+import axios from 'axios'
+
 const App = () => {
-  return <div>App</div>
+  const url = 'https://jsonplaceholder.typicode.com/users'
+
+  // GET
+  const getData = () => {
+    axios
+      .get(url)
+      .then((res) => console.log(res.data))
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  // POST
+  const postData = () => {
+    axios
+      .post(url, { name: 'rakib', age: 18 })
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e))
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>get</button>
+      <button onClick={postData}>post data</button>
+    </div>
+  )
 }
 
 export default App
